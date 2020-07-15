182f2a9b65e3a450b7dedbd44c1422d426be9c61
ayush6624 <ayushg1214@gmail.com>
2020-07-15 17:43:46 +0530

    feat: add analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,8 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { CssBaseline } from '@zeit-ui/react';
 
+let GA_TRACKING_ID = 'UA-134274140-1';
+if (process.env.NODE_ENV !== 'production') GA_TRACKING_ID = '';
 class MyDocument extends Document {
   //   static async getInitialProps(ctx) {
   //     const initialProps = await Document.getInitialProps(ctx);
@@ -21,10 +23,23 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head />
+        <>
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${GA_TRACKING_ID}');
+      `,
+            }}
+          />
+        </>
         <body>
-          {/* Possible Google Analytics, and custom analytics! */}
           <Main />
           <NextScript />
+          <img src="https://a.ayushgoyal.dev/hello.png?id=website"></img>
         </body>
       </Html>
     );
